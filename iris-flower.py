3649be6fb486deeac31a8352e2ab7ce80ed8656d
Emerson Loureiro <emerson.loureiro@gmail.com>
2016-06-25 01:10:45 +0100

    Neural network model for the iris flower dataset

--- iris-flower.py
+++ iris-flower.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class IrisNN:
@@ -13,17 +14,15 @@ class IrisNN:
 class Input:
     def __init__(self, file_path, iris_nn):
         self.current = 0
-        self.xs = []
+        xs_temp = []
         self.ys = []
         with open(file_path) as inputFile:
             csvReader = csv.reader(inputFile, delimiter=',')
             for line in csvReader:
                 x = []
-                # Bias
-                x.append(1)
                 for i in range(0, iris_nn.number_features):
                     x.append(float(line[i]))
-                self.xs.append(x)
+                xs_temp.append(x)
                 y = int(line[4])
                 ys_temp = []
                 for c in range(0, iris_nn.number_classes):
@@ -32,6 +31,19 @@ class Input:
                     else:
                         ys_temp.append(0)
                 self.ys.append(ys_temp)
+
+        self.xs = []
+        xs_temp = np.array(xs_temp)
+        xs_mean = np.mean(xs_temp, axis=0)
+        xs_std = np.std(xs_temp, axis=0)
+        for i in range(len(xs_temp)):
+            normalized_xs = []
+            # Bias
+            normalized_xs.append(1)
+            for j in range(len(xs_temp[i])):
+                normalized_x = (xs_temp[i][j] - xs_mean[j]) / xs_std[j]
+                normalized_xs.append(normalized_x)
+            self.xs.append(normalized_xs)
         self.m = len(self.xs)
         self.ys = np.transpose(self.ys)
 
@@ -53,40 +65,40 @@ iris_nn = IrisNN(3, 4, 3)
 input_data = Input('sample-datasets/iris-flower-edited.csv', iris_nn)
 
 # Weights matrix
-W = tf.Variable(tf.random_normal([iris_nn.number_hidden_units, iris_nn.number_features + 1]), name='W')
+W1 = tf.Variable(tf.random_normal([iris_nn.number_hidden_units, iris_nn.number_features + 1]), name='W1')
 
 # Input layer
 batch_size = input_data.m
 X = tf.placeholder(tf.float32, shape=[batch_size, iris_nn.number_features + 1], name='X')
 
 # Hidden layer 1
-A1 = tf.matmul(W, X, transpose_b=True, name='A1')
+A1 = tf.sigmoid(tf.matmul(W1, X, transpose_b=True), name='A1')
 
 # Output layer
-Y = tf.placeholder(tf.float32, shape=[iris_nn.number_classes, batch_size], name='Y')
-H = tf.nn.softmax(A1, name='H')
+W2 = tf.Variable(tf.random_normal([iris_nn.number_classes, iris_nn.number_hidden_units]), name='W2')
+H = tf.transpose(tf.nn.softmax(tf.transpose(tf.matmul(W2, A1))), name='H')
 
 # Cost function
-j = tf.reduce_sum(np.power((H - Y), 2)) + (2/input_data.m)
-# j = -tf.reduce_sum(Y * tf.log(H), name='j')
+Y = tf.placeholder(tf.float32, shape=[iris_nn.number_classes, batch_size], name='Y')
+j = -tf.reduce_sum(Y * tf.log(H), name='j')
 
 # Gradient descent optimizer
-optimizer = tf.train.GradientDescentOptimizer(5.0).minimize(j)
+optimizer = tf.train.GradientDescentOptimizer(0.005).minimize(j)
 
 # Training
 session = tf.Session()
 session.run(tf.initialize_all_variables())
-
-# Evaluation
-correct_prediction = tf.equal(tf.argmax(H, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-epochs = 55
+epochs = 2000
+cost = []
+iteration = []
 for epoch in range(epochs):
-    # print('Cost %.5f' % session.run(j, feed_dict={X: input_data.xs, Y: input_data.ys}))
-    # print('Accuracy %.5f' % session.run(accuracy, feed_dict={X: input_data.xs, Y: input_data.ys}))
-    print(session.run(H, feed_dict={X: input_data.xs}))
-    # print(session.run(A1, feed_dict={X: input_data.xs}))
-    # print(W.eval(session))
-    print('=====================================================================')
+    cost.append(session.run(j, feed_dict={X: input_data.xs, Y: input_data.ys}))
+    iteration.append(epoch)
     session.run(optimizer, feed_dict={X: input_data.xs, Y: input_data.ys})
+
+plt.ion()
+fig, ax = plt.subplots(1, 1)
+plt.plot(iteration, cost)
+fig.show()
+plt.draw()
+plt.waitforbuttonpress()
