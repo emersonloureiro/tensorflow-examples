83bd942cf15d3f150b2c3c5e14a5935193996a73
Emerson Loureiro <emerson.loureiro@gmail.com>
2016-06-25 22:02:57 +0100

    Refactored iris flower solution to use numpy utils for file reading
    - Also using scikit for some utils now

--- iris-flower/input.py
+++ iris-flower/input.py
@@ -1,50 +1,27 @@
-import csv
 import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import label_binarize
 
 
 class Input:
     def __init__(self, file_path, number_features, number_classes):
-        self.current = 0
-        xs_temp = []
-        self.ys = []
-        with open(file_path) as inputFile:
-            csvReader = csv.reader(inputFile, delimiter=',')
-            for line in csvReader:
-                x = []
-                for i in range(0, number_features):
-                    x.append(float(line[i]))
-                xs_temp.append(x)
-                y = int(line[4])
-                ys_temp = []
-                for c in range(0, number_classes):
-                    if y == c:
-                        ys_temp.append(1)
-                    else:
-                        ys_temp.append(0)
-                self.ys.append(ys_temp)
+        dataset = self.load_dataset(file_path, number_features)
+        xs = dataset[:, 0:number_features + 1]
+        ys = dataset[:, number_features + 1]
+        self.xs, self.xs_test, ys, self.ys_test = train_test_split(xs, ys, train_size=0.6)
+        self.ys = np.transpose(label_binarize(ys, classes=[0, 1, 2]))
+        self.m = self.xs.shape[0]
 
-        self.xs = []
-        xs_temp = np.array(xs_temp)
-        xs_mean = np.mean(xs_temp, axis=0)
-        xs_std = np.std(xs_temp, axis=0)
-        for i in range(len(xs_temp)):
-            normalized_xs = []
-            # Bias
-            normalized_xs.append(1)
-            for j in range(len(xs_temp[i])):
-                normalized_x = (xs_temp[i][j] - xs_mean[j]) / xs_std[j]
-                normalized_xs.append(normalized_x)
-            self.xs.append(normalized_xs)
-        self.m = len(self.xs)
-        self.ys = np.transpose(self.ys)
-
-    def next_batch(self, batch_size):
-        x_temp = []
-        y_temp = []
-        for i in range(self.current, self.current + batch_size):
-            if i > self.m:
-                break
-            x_temp.append(self.xs[i])
-            y_temp.append(self.ys[i])
-        self.current = self.current + batch_size
-        return x_temp, y_temp
+    def load_dataset(self, file_path, number_features):
+        dataset = np.genfromtxt(file_path, delimiter=',')
+        xs = dataset[:, 0:number_features]
+        xs_mean = np.mean(xs, axis=0)
+        xs_std = np.std(xs, axis=0)
+        m = dataset.shape[0]
+        bias = np.ones((m, 1))
+        dataset = np.append(bias, dataset, axis=1)
+        for i in range(m):
+            for j in range(1, number_features):
+                normalized_x = (dataset[i][j] - xs_mean[j - 1]) / xs_std[j - 1]
+                dataset[i][j] = normalized_x
+        return dataset
